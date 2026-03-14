9d02a2a2a96d8d145902f51e3b94ab80ff117919
Vox <wbdev.vox@gmail.com>
2026-03-14 22:52:23 +0400

    feat(tasks): реализовано создание задачи и FormRequest

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Task;
+use App\Http\Requests\StoreTaskRequest;
 
 class TaskController extends Controller
 {
-    //
+    public function store(StoreTaskRequest $request)
+    {
+        // Берем ТОЛЬКО те данные, которые прошли валидацию
+        $validateData = $request->validated();
+
+        // Передаем валидированные данные в create
+        $task = Task::create($validateData);
+
+        return response()->json([
+            'status'    => 'success',
+            'data'      => $task
+        ], 201);
+    }
 }
